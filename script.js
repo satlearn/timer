8fa759c4c0cf3a2de2cd5865eda07abaa6c2b5df
GitTech <Git.TechSat@gmail.com>
2023-08-07 12:11:06 +0100

    css and am,pm changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*TASK
 from current date and time
 /'Today is: Thursday, 22nd of June 2023*/
-let date=new Date();
+/*let date=new Date();
 console.log(date)
 
 let todayDate=new Date();
@@ -16,12 +16,12 @@ let week = todayDate1.toLocaleDateString('en-uk',{weekday:'long'});
 let mon = todayDate1.toLocaleDateString('en-uk', {month:'long'});
 let d = todayDate1.toLocaleDateString('en-us', {day:'2-digit'});
 let yr = todayDate1.getFullYear();
-console.log(`Today is: ${week} ${d}nd of ${mon} ${yr}`);
+console.log(`Today is: ${week} ${d}nd of ${mon} ${yr}`);*/
 
 //TASK-Timer
 //Current date in the browser and it should be updating regularly.
 
-function displayTime(){
+/*function displayTime(){
 let todayDate2 = new Date();
 let hourElement = document.getElementById("hour");
 let min = document.getElementById("min");
@@ -44,7 +44,38 @@ setInterval(displayTime,500);
 function padZero(num){
     return(num < 10 )?"0" + num:num;
 }
-
+*/
 /*By using hourElement.innerText instead of hourElement.value, we can retrieve the displayed hour value as a string, which we then parse as an integer using parseInt. This should ensure that the hour value is properly processed and the AM/PM indicator updates accordingly.
 parseInt(...): The parseInt function is used to convert a string representation of a number into an actual integer value. It takes two arguments: the string to be parsed and the radix/base of the number system to use for parsing. In this case, we are using a radix/base of 10, which corresponds to the decimal number system.*/
 
+function displayTime() {
+    let todayDate2 = new Date();
+    let hourElement = document.getElementById("hour");
+    let min = document.getElementById("min");
+    let second = document.getElementById("second");
+    let msecond = document.getElementById("m-second");
+    
+    let hour = parseInt(hourElement.innerText, 10); // Parse the inner text as an integer
+    let ampm = document.getElementById("ampm");
+  
+    if (hour >= 12) {
+      if (hour > 12) {
+        hour = hour - 12;
+      }
+      ampm.innerHTML = 'PM';
+    } else {
+      ampm.innerHTML = 'AM';
+    }
+  
+    hourElement.innerHTML = padZero(todayDate2.getHours());
+    min.innerHTML = padZero(todayDate2.getMinutes());
+    second.innerHTML = padZero(todayDate2.getSeconds());
+    msecond.innerHTML = todayDate2.getTime();
+  }
+  
+  setInterval(displayTime, 500);
+  
+  function padZero(num) {
+    return (num < 10) ? "0" + num : num;
+  }
+  
